18e43178841589fc1706cfc41344af36ff7aa7a7
YanhaoSun <72014576+YanhaoSun@users.noreply.github.com>
2024-10-13 22:36:12 +0100

    Add files via upload

---
+++ Lab4_lifeco_jms/task3/dodgygeezers/src/main/java/Main.java
@@ -0,0 +1,50 @@
+import org.apache.activemq.ActiveMQConnectionFactory;
+import service.core.Quotation;
+import service.dodgygeezers.DGQService;
+import service.message.ClientMessage;
+import service.message.QuotationMessage;
+
+import javax.jms.*;
+public class Main {
+    private static DGQService service = new DGQService();
+
+    public static void main(String[] args) {
+        ConnectionFactory factory =
+                new ActiveMQConnectionFactory("failover://tcp://localhost:61616");
+        Connection connection = null;
+        try {
+            connection = factory.createConnection();
+            connection.setClientID("dodgygeezers");
+            Session session = connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+
+            Queue queue = session.createQueue("QUOTATIONS");
+            Topic topic = session.createTopic("APPLICATIONS");
+            MessageConsumer consumer = session.createConsumer(topic);
+            MessageProducer producer = session.createProducer(queue);
+
+//          makes the service consume incoming ClientMessage message,
+//          generating a Quotation and then producing a QuotationMessage message
+//          that it submits to the QUOTATIONS queue.
+            consumer.setMessageListener(new MessageListener() {
+                @Override
+                public void onMessage(Message message) {
+                    try {
+                        ClientMessage request = (ClientMessage)
+                                ((ObjectMessage) message).getObject();
+                        Quotation quotation = service.generateQuotation(request.getInfo());
+                        Message response =
+                                session.createObjectMessage(
+                                        new QuotationMessage(request.getToken(), quotation));
+                        producer.send(response);
+                        message.acknowledge();
+                    } catch (JMSException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
---
+++ Lab4_lifeco_jms/task3/dodgygeezers/src/test/java/QuotationTest.java
@@ -0,0 +1,55 @@
+//import jdk.javadoc.internal.tool.Main;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.ObjectMessage;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.Topic;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import service.core.ClientInfo;
+import service.message.ClientMessage;
+import service.message.QuotationMessage;
+public class QuotationTest {
+    @Test
+    public void testService() throws Exception {
+        Main.main(new String[0]);
+
+        ConnectionFactory factory =
+                new ActiveMQConnectionFactory("failover://tcp://localhost:61616");
+        Connection connection = factory.createConnection();
+        connection.setClientID("test");
+        Session session =
+                connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+        Queue queue = session.createQueue("QUOTATIONS");
+        Topic topic = session.createTopic("APPLICATIONS");
+        MessageConsumer consumer = session.createConsumer(queue);
+        MessageProducer producer = session.createProducer(topic);
+        connection.start();
+
+
+        producer.send(
+                session.createObjectMessage(
+                        new ClientMessage(1L, new ClientInfo("Niki Collier",
+                                ClientInfo.FEMALE, 49, 1.5494, 80, false,
+                                false))));
+
+        Message message = consumer.receive();
+        QuotationMessage quotationMessage =
+                (QuotationMessage) ((ObjectMessage) message).getObject();
+
+        System.out.println("token: " + quotationMessage.getToken());
+        System.out.println("quotation: " + quotationMessage.getQuotation());
+        message.acknowledge();
+
+        assertEquals(1L, quotationMessage.getToken());
+    }
+}
