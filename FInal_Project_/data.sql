aa0e81b775df32b149a442d0191a36d6afd045ea
YanhaoSun <72014576+YanhaoSun@users.noreply.github.com>
2024-10-13 23:01:46 +0100

    Add files via upload

---
+++ FInal_Project_/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (user_id, user_name, password, age) VALUES ('1', 'yanhao', '123', '23');
+INSERT INTO users (user_id, user_name, password, age) VALUES ('2', 'spiderman', '456', '24');
+INSERT INTO users (user_id, user_name, password, age) VALUES ('3', 'ironman', '789', '25');
