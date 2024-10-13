a5376ef6fb04a28afc70663f65739005e555951a
YanhaoSun <72014576+YanhaoSun@users.noreply.github.com>
2024-10-13 22:37:18 +0100

    Add files via upload

---
+++ Lab4_lifeco_jms/task4/auldfellas/src/test/java/QuotationTest.java
@@ -0,0 +1,57 @@
+////import jdk.javadoc.internal.tool.Main;
+//import org.junit.Test;
+//
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertNotNull;
+//
+//import javax.jms.Connection;
+//import javax.jms.ConnectionFactory;
+//import javax.jms.Message;
+//import javax.jms.MessageConsumer;
+//import javax.jms.MessageProducer;
+//import javax.jms.ObjectMessage;
+//import javax.jms.Queue;
+//import javax.jms.Session;
+//import javax.jms.Topic;
+//import service.auldfellas.Main;
+//import org.apache.activemq.ActiveMQConnectionFactory;
+//import service.core.ClientInfo;
+//import service.message.ClientMessage;
+//import service.message.QuotationMessage;
+//
+//public class QuotationTest {
+//    @Test
+//    public void testService() throws Exception {
+//        Main.main(new String[0]);
+//
+//        ConnectionFactory factory =
+//                new ActiveMQConnectionFactory("failover://tcp://localhost:61616");
+//        Connection connection = factory.createConnection();
+//        connection.setClientID("test");
+//        Session session =
+//                connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+//        Queue queue = session.createQueue("QUOTATIONS");
+//        Topic topic = session.createTopic("APPLICATIONS");
+//        MessageConsumer consumer = session.createConsumer(queue);
+//        MessageProducer producer = session.createProducer(topic);
+//        connection.start();
+//
+//        System.out.println("123");
+//        producer.send(
+//                session.createObjectMessage(
+//                        new ClientMessage(1L, new ClientInfo("Niki Collier",
+//                                ClientInfo.FEMALE, 49, 1.5494, 80, false,
+//                                false))));
+//        System.out.println("45adasfdasfasfasfasf6");
+//        Message message = consumer.receive();
+//        System.out.println("789");
+//        QuotationMessage quotationMessage =
+//                (QuotationMessage) ((ObjectMessage) message).getObject();
+//
+//        System.out.println("token: " + quotationMessage.getToken());
+//        System.out.println("quotation: " + quotationMessage.getQuotation());
+//        message.acknowledge();
+//
+//        assertEquals(1L, quotationMessage.getToken());
+//    }
+//}
---
+++ Lab4_lifeco_jms/task4/client/src/main/java/service/client/Main.java
@@ -0,0 +1,107 @@
+package service.client;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import service.core.ClientInfo;
+import service.core.Quotation;
+import service.message.ClientMessage;
+import service.message.OfferMessage;
+import service.message.QuotationMessage;
+
+import java.text.NumberFormat;
+import java.util.concurrent.atomic.AtomicLong;
+import javax.jms.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ConnectionFactory factory =
+                new ActiveMQConnectionFactory("failover://tcp://localhost:61616");
+        Connection connection = null;
+        try {
+            connection = factory.createConnection();
+            connection.setClientID("client");
+            Session session = connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+            Queue queue = session.createQueue("OFFERS");
+            Topic topic = session.createTopic("APPLICATIONS");
+
+            MessageProducer producer = session.createProducer(topic);
+            MessageConsumer consumer = session.createConsumer(queue);
+            AtomicLong counter = new AtomicLong();
+            connection.start();
+
+            for (ClientInfo info : clients) {
+                long token = counter.incrementAndGet();
+                Message request =
+                        session.createObjectMessage(
+                                new ClientMessage(token, info));
+                producer.send(request);
+                consumer.setMessageListener(new MessageListener() {
+                    @Override
+                    public void onMessage(Message message) {
+                        try {
+                            OfferMessage response = (OfferMessage)
+                                    ((ObjectMessage) message).getObject();
+                            // Retrieve quotations from the broker and display them...
+                            displayProfile(response.getInfo());
+                            for(Quotation quotation : response.getQuotations()) {
+                                displayQuotation(quotation);
+                            }
+                            System.out.println("\n");
+                            message.acknowledge();
+                        } catch (JMSException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                // Print a couple of lines between each client
+            }
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    /**
+     * Display the client info nicely.
+     *
+     * @param info
+     */
+    public static void displayProfile(ClientInfo info) {
+        System.out.println("|=================================================================================================================|");
+        System.out.println("|                                     |                                     |                                     |");
+        System.out.println(
+                "| Name: " + String.format("%1$-29s", info.name) +
+                        " | Gender: " + String.format("%1$-27s", (info.gender==ClientInfo.MALE?"Male":"Female")) +
+                        " | Age: " + String.format("%1$-30s", info.age)+" |");
+        System.out.println(
+                "| Weight/Height: " + String.format("%1$-20s", info.weight+"kg/"+info.height+"m") +
+                        " | Smoker: " + String.format("%1$-27s", info.smoker?"YES":"NO") +
+                        " | Medical Problems: " + String.format("%1$-17s", info.medicalIssues?"YES":"NO")+" |");
+        System.out.println("|                                     |                                     |                                     |");
+        System.out.println("|=================================================================================================================|");
+    }
+
+    /**
+     * Display a quotation nicely - note that the assumption is that the quotation will follow
+     * immediately after the profile (so the top of the quotation box is missing).
+     *
+     * @param quotation
+     */
+    public static void displayQuotation(Quotation quotation) {
+        System.out.println(
+                "| Company: " + String.format("%1$-26s", quotation.company) +
+                        " | Reference: " + String.format("%1$-24s", quotation.reference) +
+                        " | Price: " + String.format("%1$-28s", NumberFormat.getCurrencyInstance().format(quotation.price))+" |");
+        System.out.println("|=================================================================================================================|");
+    }
+
+    /**
+     * Test Data
+     */
+    public static final ClientInfo[] clients = {
+            new ClientInfo("Niki Collier", ClientInfo.FEMALE, 49, 1.5494, 80, false, false),
+            new ClientInfo("Old Geeza", ClientInfo.MALE, 65, 1.6, 100, true, true),
+            new ClientInfo("Hannah Montana", ClientInfo.FEMALE, 21, 1.78, 65, false, false),
+            new ClientInfo("Rem Collier", ClientInfo.MALE, 49, 1.8, 120, false, true),
+            new ClientInfo("Jim Quinn", ClientInfo.MALE, 55, 1.9, 75, true, false),
+            new ClientInfo("Donald Duck", ClientInfo.MALE, 35, 0.45, 1.6, false, false)
+    };
+}
